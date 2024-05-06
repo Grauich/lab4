bf377cf3b0b14f1bc582d4c92b8801ce867a1203
Grauich <118853584+Grauich@users.noreply.github.com>
2024-05-06 17:52:57 +0300

    Update 4lab3.py

--- 4lab3.py
+++ 4lab3.py
@@ -1,8 +1,6 @@
 def magic(date):
     try:
-        day, month, year = map(
-            int, date.split(".")
-        )  # оставлю тут, split это разделитель
+        day, month, year = map(int, date.split("."))  # оставлю тут, split это разделитель
         return day * month == year % 100
     except ValueError:
         return False
